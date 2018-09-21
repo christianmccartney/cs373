1df97eaf5729f7545ecf4bd3300b0cd37d6d4f11
christianmccartney <mccartney.christian@gmail.com>
2018-09-20 20:30:00 -0500

    Upload project 1

---
+++ project1/RunCollatz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# -------------
+# RunCollatz.py
+# -------------
+
+# -------
+# imports
+# -------
+
+from sys     import stdin, stdout
+from Collatz import collatz_solve
+
+# ----
+# main
+# ----
+
+if __name__ == "__main__" :
+    collatz_solve(stdin, stdout)
